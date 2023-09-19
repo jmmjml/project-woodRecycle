8b364db9f33c877bd069f1abec5055585da6cfac
Paulo Henrique <paulohsangelo@gmail.com>
2023-09-19 10:37:22 -0300

    Ubuntu Font added

--- components/Fonts.js
+++ components/Fonts.js
@@ -1,5 +1,11 @@
 import { loadAsync } from "expo-font";
-
+import Ubuntu from '../assets/fonts/Ubuntu-Regular.ttf'
+import UbuntuBold from '../assets/fonts/Ubuntu-Bold.ttf'
+import UbuntuMedium from '../assets/fonts/Ubuntu-Medium.ttf'
 export default Fonts = async () => {
-  await loadAsync({});
+  await loadAsync({
+    'Ubuntu': Ubuntu;
+    'UbuntuMedium': UbuntuMedium;
+    "UbuntuBold": UbuntuBold; 
+  });
 };
